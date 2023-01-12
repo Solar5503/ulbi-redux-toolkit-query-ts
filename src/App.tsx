fb3a10e9e65adb7a79ae9f398f685f4ffa825e22
Solar5503 <spidervvv@mail.ru>
2023-01-12 16:27:07 +0300

    add counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
-  return <div className="App">Work!</div>;
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(10))}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import { setupStore } from './store/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const store = setupStore();
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
