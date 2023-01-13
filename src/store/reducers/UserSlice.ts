2264fb630e16cd0fb1ed4e75338b8f1e735c4bf6
Solar5503 <spidervvv@mail.ru>
2023-01-13 17:47:08 +0300

    add modern way of rtk

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -18,6 +18,7 @@ export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    //? Способ по умолчанию
     // userFetching(state) {
     //   state.isLoading = true;
     // },
@@ -31,20 +32,40 @@ export const userSlice = createSlice({
     //   state.error = action.payload;
     // },
   },
-  extraReducers: {
-    [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
-      state.isLoading = false;
-      state.error = '';
-      state.users = action.payload;
-    },
-    [fetchUsers.pending.type]: (state) => {
-      state.isLoading = true;
-    },
-    [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+  extraReducers: (builder) => {
+    //^ Новый способ
+    builder
+      .addCase(
+        fetchUsers.fulfilled,
+        (state, action: PayloadAction<IUser[]>) => {
+          state.isLoading = false;
+          state.error = '';
+          state.users = action.payload;
+        }
+      )
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUsers.rejected, (state, action: PayloadAction<unknown>) => {
+        state.isLoading = false;
+        state.error = action.payload as string;
+      });
   },
+  //   {
+  //     //& Старый способ
+  //     [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
+  //       state.isLoading = false;
+  //       state.error = '';
+  //       state.users = action.payload;
+  //     },
+  //     [fetchUsers.pending.type]: (state) => {
+  //       state.isLoading = true;
+  //     },
+  //     [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
+  //       state.isLoading = false;
+  //       state.error = action.payload;
+  //     },
+  //   },
 });
 
 export default userSlice.reducer;
