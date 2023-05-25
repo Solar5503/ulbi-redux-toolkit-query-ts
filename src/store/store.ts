2220d73106d14e08f5c12ef8301c42b1b97474c6
Solar5503 <spidervvv@mail.ru>
2023-05-25 17:29:33 +0300

    fix type of dispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { postAPI } from '../services/PostService';
 import userReducer from './reducers/UserSlice';
 
-// export const setupStore = () => {
-//   return configureStore({ reducer: rootReducer });
-// };
 export const store = configureStore({
   reducer: {
     userReducer,
@@ -15,5 +12,5 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppStore = typeof store;
-export type AppDispatch = AppStore['dispatch'];
+
+export type AppDispatch = typeof store.dispatch;
