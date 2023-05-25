51445d1b7fe60a9926333970b438a0c71e0dc12b
Solar5503 <spidervvv@mail.ru>
2023-05-25 17:15:20 +0300

    fix type of state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { postAPI } from '../services/PostService';
 import userReducer from './reducers/UserSlice';
-
-const rootReducer = combineReducers({
-  userReducer,
-  [postAPI.reducerPath]: postAPI.reducer,
-});
 
 // export const setupStore = () => {
 //   return configureStore({ reducer: rootReducer });
 // };
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    userReducer,
+    [postAPI.reducerPath]: postAPI.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(postAPI.middleware),
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppStore = typeof store;
 export type AppDispatch = AppStore['dispatch'];
